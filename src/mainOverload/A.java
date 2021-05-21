0beb76d89eb8917a5197aad6a7946f377ccecf4c
Venkateswara Rao Panganamamula <venkyrojavishnu@Venkateswaras-MacBook-Air.local>
2021-05-21 11:06:18 -0400

    added additional examples

--- src/mainOverload/A.java
+++ src/mainOverload/A.java
@@ -1,8 +1,25 @@
 package mainOverload;
 
 public class A {
-
+    
 	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Message A");
+		
+		try {
+			
+			throw new Exception("Raja's exception");
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		System.out.println("Message B");
+		
+		
 		System.out.println("\n=================================== Begin ===mainMethodOverloading -- Class A========================\n");
 		
 		System.out.println("This is main method of Class A");
@@ -18,8 +35,15 @@ public class A {
 		//mainMethodOverloading.main("Raja");	
 		
 		
+		
 		System.out.println("\n=================================== End ===mainMethodOverloading -- Class A========================\n");
 
+		
+		System.out.println("\n after handling the exception...");
+	
+	
 	}
+	
+
 
 }
